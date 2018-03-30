6008fa1e44033aa8fb2062ebf27a8b5bba9cfe7d
RudaMo <37925980+RudaMo@users.noreply.github.com>
2018-03-30 10:02:44 +0200

    Add files via upload

---
+++ pierwszy.py
@@ -0,0 +1,18 @@
+#! /Users/Monika/Projekty/kurs/venv/bin/python
+# -*- coding: utf-8 -*-
+
+imię = input("Jak masz na imię? - ")
+
+print(f"Cześć {imię}")
+
+wiek = input("Ile masz lat? - ")
+
+print("Z jakiego peronu odjeżdża pociąg do Hogwaru?")
+
+peron = input("- ")
+print(peron)
+if peron == "9 3/4":
+ print("Brawo ! Możesz kupować bilet na pociąg !!!")
+
+else:
+ print("Niestety zdaje się, że jesteś Mugolem :P")
